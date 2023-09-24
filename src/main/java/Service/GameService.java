61bd1e12eb2f0d432603f6c1f912b2777d445b06
abdelghafour aouad <abdelghafour.aouad0@gmail.com>
2023-09-24 21:56:44 +0100

    created necessaire methods

--- src/main/java/Service/GameService.java
+++ src/main/java/Service/GameService.java
@@ -1,4 +1,104 @@
-package Service;
+package service;
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
 
 public class GameService {
+    private static final int[] RANKS = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
+    private static final int[] SUITS = {1, 2, 3, 4}; // Hearts, Diamonds, Clubs, Spades
+
+    public static void main(String[] args) {
+        int[][] deck = createDeck();
+
+        // Testing: Print the deck to verify it's created correctly
+        System.out.println("The deck created:");
+        Arrays.stream(deck).map(Arrays::toString).forEach(System.out::println);
+        System.out.println("Number of cards in deck: " + deck.length);
+
+        // Testing: Print the deck to verify it's shuffled correctly and no cards are missing with number of cards in deck
+        System.out.println("The deck shuffled:");
+        Arrays.stream(shuffleDeck(deck)).map(Arrays::toString).forEach(System.out::println);
+        System.out.println("Number of cards in deck: " + deck.length);
+
+        // Testing: Print the deck sorted by rank
+        System.out.println("The deck sorted by rank:");
+        Arrays.stream(sortDeckByRank(deck)).map(Arrays::toString).forEach(System.out::println);
+        System.out.println("Number of cards in deck: " + deck.length);
+    }
+
+    public static int[][] createDeck() {
+        return IntStream.range(0, RANKS.length * SUITS.length)// 0 to 52
+                .mapToObj(index -> new int[]{RANKS[index % RANKS.length], SUITS[index / RANKS.length]})
+                .toArray(int[][]::new);
+    }
+
+    public static Object[] extractCardByIndex(int[][] deck, int index) {
+        if (index < 0 || index >= deck.length) {
+            // Handle invalid index here, such as returning an error or throwing an exception
+            // ...
+            return null;
+        }
+
+        Object[] newDeck = new Object[2];
+        newDeck[0] = deck[index];
+
+        int[][] newDeck2 = new int[deck.length - 1][deck[0].length];
+        for (int i = 0, newIndex = 0; i < deck.length; i++) {
+            if (i != index) {
+                newDeck2[newIndex++] = deck[i];
+            }
+        }
+        newDeck[1] = newDeck2;
+        return newDeck;
+    }
+
+
+    public static Object[] extractRandomCard(int[][] deck) {
+        int index = (int) (Math.random() * deck.length);
+        return extractCardByIndex(deck, index);
+    }
+
+    public static int[][] shuffleDeck(int[][] deck) {
+        int[][] shuffledDeck = new int[deck.length][deck[0].length];
+        int[][] tmpDeck = deck.clone();
+        for (int i = 0; i < deck.length; i++) {
+            Object[] card = extractRandomCard(tmpDeck);
+            shuffledDeck[i] = (int[]) card[0];
+            tmpDeck = (int[][]) card[1];
+        }
+        return shuffledDeck;
+    }
+
+    public static int[][] sortDeckByRank(int[][] deck) {
+        int[][] sortedDeck = new int[deck.length][deck[0].length];
+        int[][] tmpDeck = deck.clone();
+
+        // Implement a sorting algorithm based on the rank
+        for (int i = 0; i < deck.length; i++) {
+            int minIndex = findMinRankCardIndex(tmpDeck);
+            Object[] card = extractCardByIndex(tmpDeck, minIndex);
+            sortedDeck[i] = (int[]) card[0];
+            tmpDeck = (int[][]) card[1];
+        }
+
+        return sortedDeck;
+    }
+
+    private static int findMinRankCardIndex(int[][] deck) {
+        int minIndex = 0;
+        int minRank = deck[0][0]; // Initialize with the rank of the first card
+
+        for (int i = 1; i < deck.length; i++) {
+            int currentRank = deck[i][0];
+            if (currentRank < minRank) {
+                minRank = currentRank;
+                minIndex = i;
+            }
+        }
+
+        return minIndex;
+    }
+    
+
+
 }
