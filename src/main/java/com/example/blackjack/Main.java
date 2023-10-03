dbce173ff32935e7ad698cc1cdf362d0d1837601
abdelghafour <miso.abdo@gmail.com>
2023-10-03 23:27:07 +0100

    refactoring structure of view

--- src/main/java/com/example/blackjack/Main.java
+++ src/main/java/com/example/blackjack/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("fxml/start-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setFullScreen(true);
+//        stage.setFullScreen(true);
 //        stage.setResizable(false);
         stage.setTitle("BlackJack");
        // stage.getIcons().add(new Image("com/example/blackjack/images/icon.png"));
--- src/main/java/com/example/blackjack/controller/MainController.java
+++ src/main/java/com/example/blackjack/controller/MainController.java
@@ -7,7 +7,9 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
@@ -16,8 +18,12 @@ import java.util.Objects;
 
 public class MainController {
 
+//    @FXML
+//    private ImageView back_card;
+
     @FXML
-    private ImageView back_card;
+    private StackPane cards;
+
 
     @FXML
     public void gameView(ActionEvent event) throws IOException {
@@ -27,26 +33,44 @@ public class MainController {
         Scene gameViewScene = new Scene(gameViewRoot);
 
 
-        // Access the primary stage from the event and set the new scene
         Stage primaryStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
 
         primaryStage.setScene(gameViewScene);
-        primaryStage.setFullScreen(true);
+//        primaryStage.setFullScreen(true);
 
-        TranslateTransition transition = new TranslateTransition();
-        transition.setDuration(Duration.seconds(0.1));
-        transition.setNode(back_card);
+//        TranslateTransition transition = new TranslateTransition();
+//        transition.setDuration(Duration.seconds(0.1));
+
+
+
         gameViewScene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("/com/example/blackjack/css/style.css")).toExternalForm());
 
-        transition.setToX(-20);
-        transition.setAutoReverse(true);
-        transition.setCycleCount(TranslateTransition.INDEFINITE);
-        PauseTransition pauseTransition = new PauseTransition(Duration.seconds(1));
-        pauseTransition.setOnFinished(even -> {
+        primaryStage.show();
+        animationShufflingCards();
+//        transition.setToX(-20);
+//        transition.setAutoReverse(true);
+//        transition.setCycleCount(TranslateTransition.INDEFINITE);
+//        PauseTransition pauseTransition = new PauseTransition(Duration.seconds(1));
+//        pauseTransition.setOnFinished(even -> {
 //            back_card.getStyleClass().add("rotation");
 //            transition.play();
-        });
-        pauseTransition.play();
+//        });
+//        pauseTransition.play();
+    }
+
+    @FXML
+    public void animationShufflingCards(){
+        System.out.println("Shuffling cards");
+
+        ImageView card = new ImageView(new Image(String.valueOf(getClass().getResource("/com/example/blackjack/images/cards/card_back.png"))));
+        card.setFitHeight(150);
+        card.setFitWidth(100);
+        card.getStyleClass().add("rotation");
+        cards.getChildren().add(card);
+
+
+
+
     }
 
 
